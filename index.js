044277feab51de49edaef5b6d4403a2c8958a34e
unknown <abdullahalomar048@gmail.com>
2022-05-27 02:48:43 +0600

    database connect on client side

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const app = express()
+const app = express();
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.57bag.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Doctors Portal!!')
